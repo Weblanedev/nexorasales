e0f7c5e3d4e82fae3525345acba69285d0d6c609
Bunmi Oye <bunmioye09@gmail.com>
2025-02-07 22:22:42 +0100

    initial commit

---
+++ src/data/brand-data.ts
@@ -0,0 +1,10 @@
+export const brand_data = {
+  name: 'Nexora',
+  full_name: 'Nexora Limited',
+  slogan: 'Compute The Future',
+  description: 'Nexora is your one-stop-shop for your global computing needs.',
+  phone: '09088199089',
+  email: 'finance@nexorasales.com',
+  address: '',
+  map: '',
+};
